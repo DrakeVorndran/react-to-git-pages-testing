05f9cd63813c0fbc8787422110c3917a76305257
Ki Vorndran <drake.vorndran@students.makeschool.com>
2019-08-28 09:41:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MyComponent from './MyComponent'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MyComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MyComponent.css'
+
+function MyComponent() {
+  return (
+    <div className='my-component'>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default MyComponent
