4139f37fa3087f357c9c5ab32c18d9061cdf1239
Ki Vorndran <drake.vorndran@students.makeschool.com>
2019-08-28 10:28:36 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import MyComponent from './MyComponent'
@@ -11,15 +10,16 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          
-            <Route exact path="/hello" render={() => <MyComponent text="Hello World"/>}/>
-            <Route exact path="/goodbye" render={() => <MyComponent text="Goodbye World"/>}/>
-            <Route exact path="/orange" render={() => <MyComponent text="I like oranges"/>}/>
-          
+          <Switch>
+            <Route exact path="/hello" render={() => <MyComponent text="Hello World" />} />
+            <Route exact path="/goodbye" render={() => <MyComponent text="Goodbye World" />} />
+            <Route exact path="/orange" render={() => <MyComponent text="I like oranges" />} />
+          </Switch>
+
           <Link to="hello">hi</Link>
           <Link to="goodbye">bye</Link>
           <Link to="orange">fruit</Link>
-          
+
         </header>
       </div>
     </Router>
